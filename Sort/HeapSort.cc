7b2558e0f4680535c9b3dd147f229c122fd9b581
fang_0099 <fang_yonghao@163.com>
2014-08-17 13:43:10 +0800

    sort algorithm

---
+++ Sort/HeapSort.cc
@@ -0,0 +1,42 @@
+//堆排序
+void HeapAdjust(int a[], int pos, int length)
+{
+	int child = 2 * pos + 1;
+	int temp;
+
+	for(int p = pos; child<length; child = 2 * p + 1)
+	{
+		if((child+1)<length && a[child+1]>a[child])
+			++child;
+		if(a[p]<a[child])
+		{
+			temp = a[p];
+			a[p] = a[child];
+			a[child] = temp;
+		}
+		else
+			break;
+
+		p = child;
+	}
+
+}
+
+void HeapSort(int a[], int length)
+{
+	int n = (length - 1)/2;
+	for(;n>=0;--n)
+	{
+		HeapAdjust(a,n,length);
+	}
+
+	while(length>1)
+	{
+		int temp = a[0];
+		a[0] = a[length - 1];
+		a[length - 1] = temp;
+
+		--length;
+		HeapAdjust(a,0,length);
+	}
+}
---
+++ Sort/MergeSort.cc
@@ -0,0 +1,42 @@
+//归并排序
+void MergeArray(int list1[], int list1_size, int list2[], int list2_size)
+{
+	int *list = (int*)malloc((list1_size + list2_size)*sizeof(int));
+	int list1_index = 0;
+	int list2_index = 0;
+	int list_index = 0;
+	while(list1_index<list1_size && list2_index<list2_size)
+	{
+		if(list1[list1_index]<list2[list2_index])
+			list[list_index++] = list1[list1_index++];
+		else
+			list[list_index++] = list2[list2_index++];
+	}
+
+	while(list1_index<list1_size)
+	{
+		list[list_index++] = list1[list1_index++];
+	}
+	while(list2_index<list2_size)
+	{
+		list[list_index++] = list2[list2_index++];
+	}
+
+	for(int i = 0; i <list1_size+list2_size; i++)
+	{
+		list1[i] = list[i];
+	}
+}
+
+void MergeSort(int a[], int length)
+{
+	if(a == NULL || length <= 1)
+		return;
+	int list1_size = length/2;
+	int list2_size = length - list1_size;
+
+	MergeSort(a,list1_size);
+	MergeSort(a+list1_size, list2_size);
+	MergeArray(a,list1_size,a+list1_size, list2_size);
+
+}
