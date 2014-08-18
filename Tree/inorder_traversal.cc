ced6e60b99f16533c91190453c253599dbbe89d6
fang_0099 <fang_yonghao@163.com>
2014-08-18 09:17:25 +0800

    indent

--- Tree/inorder_traversal.cc
+++ Tree/inorder_traversal.cc
@@ -1,10 +1,10 @@
 /**
  * Definition for binary tree
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *	   int val;
+ *	   TreeNode *left;
+ *	   TreeNode *right;
+ *	   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
 void inorderTraversal(TreeNode *root) 
